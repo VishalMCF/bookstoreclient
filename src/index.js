382d097e451bd62d343dc5e08729b59142ea9286
VishalMCF <vishaly650@gmail.com>
2022-03-30 18:17:59 +0530

    added a testcase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 
-const App = () => {
-    return (
-        <div>
-            My First Component!!!
-        </div>
-    )
-}
+import App from './App'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
